adbb95403a2b1b84fdbc7f47981690285dc5ccf6
Mark Robert <143669058+mratamayo-tsatinc@users.noreply.github.com>
2026-07-27 16:57:48 +0800

    Create 1J.c

---
+++ exercises/1J.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    print("        @       \n");
+    print("     @  @  @    \n");
+    print("  @  @  @  @  @ \n");
+    print("     @  @  @    \n");
+    print("        @       \n");
+
+    return 0;
+}
